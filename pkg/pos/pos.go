21a0b14f03ff41172fb55a9474b6e2e7a22852a2
Michalis Kargakis <kargakis@protonmail.ch>
2019-11-17 12:51:27 +0000

    Write leading zeroes in stored nums

--- pkg/pos/pos.go
+++ pkg/pos/pos.go
@@ -120,7 +120,8 @@ func WritePlotFile(filename string, k uint64, memo, id []byte) error {
 	for x := uint64(0); x < uint64(math.Pow(2, float64(k))); x++ {
 		f1x := f1.Calculate(x)
 		// TODO: Batch writes
-		_, err := file.Write([]byte(fmt.Sprintf("%d,%d\n", f1x, x)))
+		fullyPrint := fmt.Sprintf("%%0%dd,%%0%dd\n", k, k)
+		_, err := file.Write([]byte(fmt.Sprintf(fullyPrint, f1x, x)))
 		if err != nil {
 			return err
 		}
