b42bd5ada24ac4196e76752029e4201f59ae0572
Michalis Kargakis <kargakis@protonmail.ch>
2019-11-22 21:54:13 +0000

    Add BytesToUint64 unit test

--- pkg/utils/bits/bits_test.go
+++ pkg/utils/bits/bits_test.go
@@ -14,5 +14,19 @@ func TestUint64ToBytes(t *testing.T) {
 	if bigN.Uint64() != n {
 		t.Errorf("expected big.Int(n) to be %d, got %d", n, bigN.Uint64())
 	}
+}
 
+func TestBytesToUint64(t *testing.T) {
+	bigN := big.NewInt(10000000000)
+	nBytes := make([]byte, 8)
+	bigNBytes := bigN.Bytes()
+	// TODO: Zero-padding needs to move into an utility
+	for i := 8 - len(bigNBytes); i < 8; i++ {
+		nBytes[i] = bigNBytes[i-(8-len(bigNBytes))]
+	}
+
+	n := bits.BytesToUint64(nBytes)
+	if bigN.Uint64() != n {
+		t.Errorf("expected n to be %d, got %d", bigN.Uint64(), n)
+	}
 }
