d981bb3492f66f3720315ee503b15eea258757a6
Michalis Kargakis <kargakis@protonmail.ch>
2019-11-23 19:25:44 +0000

    Add memory usage utility

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"crypto/aes"
+	"fmt"
 	"math/big"
+	"runtime"
 
 	"github.com/kargakis/gochia/pkg/parameters"
 )
@@ -55,4 +57,20 @@ func FillToBlock(plain []byte) []byte {
 
 	leading := make([]byte, aes.BlockSize-remainder)
 	return append(leading, plain...)
+}
+
+// PrintMemUsage outputs the current, total and OS memory being used, as well
+// as the number of garage collection cycles completed.
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
