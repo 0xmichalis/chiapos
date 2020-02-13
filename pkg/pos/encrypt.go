650ce42b880f14f500890a562f2e2669b05962cf
Michalis Kargakis <kargakis@protonmail.ch>
2020-02-13 21:15:34 +0100

    Fix encryption in later tables

--- pkg/pos/encrypt.go
+++ pkg/pos/encrypt.go
@@ -1,6 +1,7 @@
 package pos
 
 import (
+	"crypto/aes"
 	"crypto/cipher"
 	"math/big"
 
@@ -10,7 +11,7 @@ import (
 // At is a high-level hash function that calls AES on its inputs.
 // c is meant to be created using the plot seed as a key.
 func At(x, y *big.Int, k uint64, t int, c cipher.Block) (uint64, error) {
-	param := big.NewInt(1).Lsh(big.NewInt(1), 128)
+	param := new(big.Int).Lsh(big.NewInt(1), 128)
 
 	// setup x low and high
 	xLow := new(big.Int)
@@ -30,48 +31,48 @@ func At(x, y *big.Int, k uint64, t int, c cipher.Block) (uint64, error) {
 	size := 2 * int(k) * *collaSize
 
 	// main logic
-	var cipherText []byte
+	var cipherText [aes.BlockSize]byte
 	switch {
 	case 0 <= size && size <= 128:
-		c.Encrypt(cipherText, utils.ConcatBig(k, x, y).Bytes())
+		c.Encrypt(cipherText[:], utils.FillToBlock(utils.ConcatBig(k, x, y).Bytes()))
 
 	case 129 <= size && size <= 256:
-		c.Encrypt(cipherText, x.Bytes())
-		tmp := new(big.Int).SetBytes(cipherText)
-		c.Encrypt(cipherText, tmp.Xor(tmp, y).Bytes())
+		c.Encrypt(cipherText[:], x.Bytes())
+		tmp := new(big.Int).SetBytes(cipherText[:])
+		c.Encrypt(cipherText[:], utils.FillToBlock(tmp.Xor(tmp, y).Bytes()))
 
 	case 257 <= size && size <= 384:
-		var cipherConcat []byte
-		c.Encrypt(cipherConcat, utils.ConcatBig(k, xLow, yLow).Bytes())
-		ccBig := new(big.Int).SetBytes(cipherConcat)
+		var cipherConcat [aes.BlockSize]byte
+		c.Encrypt(cipherConcat[:], utils.FillToBlock(utils.ConcatBig(k, xLow, yLow).Bytes()))
+		ccBig := new(big.Int).SetBytes(cipherConcat[:])
 
-		var cipherYHigh []byte
-		c.Encrypt(cipherYHigh, yHigh.Bytes())
-		cyBig := new(big.Int).SetBytes(cipherYHigh)
+		var cipherYHigh [aes.BlockSize]byte
+		c.Encrypt(cipherYHigh[:], utils.FillToBlock(yHigh.Bytes()))
+		cyBig := new(big.Int).SetBytes(cipherYHigh[:])
 
-		var cipherXHigh []byte
-		c.Encrypt(cipherXHigh, xHigh.Bytes())
-		cxBig := new(big.Int).SetBytes(cipherXHigh)
+		var cipherXHigh [aes.BlockSize]byte
+		c.Encrypt(cipherXHigh[:], utils.FillToBlock(xHigh.Bytes()))
+		cxBig := new(big.Int).SetBytes(cipherXHigh[:])
 
 		ccBig.Xor(ccBig, cyBig).Xor(ccBig, cxBig)
-		c.Encrypt(cipherText, ccBig.Bytes())
+		c.Encrypt(cipherText[:], utils.FillToBlock(ccBig.Bytes()))
 
 	case 385 <= size && size <= 512:
-		var tmp []byte
-		c.Encrypt(tmp, xHigh.Bytes())
-		tmpBig := new(big.Int).SetBytes(tmp)
-		c.Encrypt(tmp, tmpBig.Xor(tmpBig, xLow).Bytes())
-		tmpBig = new(big.Int).SetBytes(tmp)
+		var tmp [aes.BlockSize]byte
+		c.Encrypt(tmp[:], utils.FillToBlock(xHigh.Bytes()))
+		tmpBig := new(big.Int).SetBytes(tmp[:])
+		c.Encrypt(tmp[:], utils.FillToBlock(tmpBig.Xor(tmpBig, xLow).Bytes()))
+		tmpBig = new(big.Int).SetBytes(tmp[:])
 
-		var cipherYHigh []byte
-		c.Encrypt(cipherYHigh, yHigh.Bytes())
-		cyBig := new(big.Int).SetBytes(cipherYHigh)
+		var cipherYHigh [aes.BlockSize]byte
+		c.Encrypt(cipherYHigh[:], utils.FillToBlock(yHigh.Bytes()))
+		cyBig := new(big.Int).SetBytes(cipherYHigh[:])
 
-		c.Encrypt(cipherText, tmpBig.Xor(tmpBig, cyBig).Xor(tmpBig, yLow).Bytes())
+		c.Encrypt(cipherText[:], utils.FillToBlock(tmpBig.Xor(tmpBig, cyBig).Xor(tmpBig, yLow).Bytes()))
 	}
 
 	// need to return the most significant k+paramEXT bits
-	res := new(big.Int).SetBytes(cipherText)
+	res := new(big.Int).SetBytes(cipherText[:])
 	r := utils.Trunc(res, 0, k+5, k).Uint64()
 	return r, nil
 }
