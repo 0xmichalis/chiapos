3f856291f42437cdc001dfbe6c64d6b847e42b1b
Michalis Kargakis <kargakis@protonmail.ch>
2020-03-07 17:36:30 +0100

    Preliminary checkpoints implementation

--- pkg/parameters/parameters.go
+++ pkg/parameters/parameters.go
@@ -1,20 +1,25 @@
 package parameters
 
 const (
+	// ParamEXT defines the additional bits to be added to any function
+	// output to reduce the impact of collisions on the matching function.
 	ParamEXT = 5
-
-	ParamM = 1 << ParamEXT
+	ParamM   = 1 << ParamEXT
 
 	// B and C groups which constitute a bucket, or BC group. These groups determine how
 	// elements match with each other. Two elements must be in adjacent buckets to match.
-	ParamB = 60
-	ParamC = 509
-
+	ParamB  = 60
+	ParamC  = 509
 	ParamBC = ParamB * ParamC
+
+	// ParamC1 defines how many entries to checkpoint from the last table
+	// to enable fast lookups.
+	ParamC1 = 10000
+
+	// Space parameters controlling the plot size.
 
 	// Must be set high enough to prevent attacks of fast plotting
 	KMinPlotSize = 19
-
 	// Set at 59 to allow easy use of 64 bit integers
 	KMaxPlotSize = 59
 )
--- pkg/pos/checkpoint.go
+++ pkg/pos/checkpoint.go
@@ -1,10 +1,59 @@
 package pos
 
-import "github.com/spf13/afero"
+import (
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/spf13/afero"
+
+	"github.com/kargakis/chiapos/pkg/parameters"
+	"github.com/kargakis/chiapos/pkg/serialize"
+)
 
 // Checkpoint reads the last table in the plot and creates a new
 // table where it stores checkpoints to the last table so fast
 // retrieval of proofs can be enabled by reading the checkpoints.
 func Checkpoint(file afero.File, k int) (int, error) {
-	return 0, nil
+	fmt.Println("Starting checkpointing...")
+	var wrote int
+
+	_, start, end, err := getLastTableIndexAndPositions(file)
+	if err != nil {
+		return wrote, err
+	}
+
+	var bytesRead, read, count int
+	var entry *serialize.Entry
+	entryLen := serialize.EntrySize(k, 7)
+	for {
+		// Create checkpoints of the last table every C1 entries
+		entry, bytesRead, err = serialize.Read(file, int64(start+read), entryLen, k)
+		if errors.Is(err, serialize.EOTErr) || errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return wrote, fmt.Errorf("cannot read left entry: %w", err)
+		}
+		count++
+
+		if count%parameters.ParamC1 == 0 {
+			// Write down the exact position of the checkpointed entry in the plot.
+			pos := uint64(start + read)
+			w, err := serialize.Write(file, int64(end+1+wrote), entry.Fx, &pos, nil, nil, nil, k)
+			if err != nil {
+				return wrote + w, err
+			}
+			wrote += w
+		}
+		read += bytesRead
+	}
+
+	// TODO: Set a different index than 8, change index to a string
+	if err := updateLastTableIndexAndPositions(file, 8, end+1, end+1+wrote); err != nil {
+		return wrote, err
+	}
+	fmt.Println("Finished checkpointing...")
+
+	return wrote, nil
 }
