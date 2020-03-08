aea4c903b9e4675e94960804a2acb0e3f086e91c
Michalis Kargakis <kargakis@protonmail.ch>
2020-03-08 21:07:36 +0100

    Persist random challenges

--- cmd/prover/main.go
+++ cmd/prover/main.go
@@ -4,13 +4,14 @@ import (
 	"crypto/rand"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/kargakis/chiapos/pkg/pos"
 )
 
 var (
-	c        = flag.String("chall", "", "Challenge to use for the space proof")
+	c        = flag.String("c", "", "Challenge to use for the space proof")
 	plotPath = flag.String("f", "plot.dat", "Final path to the plot")
 )
 
@@ -25,6 +26,10 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("Using random challenge %s\n", challenge)
+		if err := ioutil.WriteFile(".random_challenge", challenge, 0777); err != nil {
+			fmt.Printf("Cannot persist random challenge: %v\n", err)
+			os.Exit(1)
+		}
 	}
 	if len(challenge) != 32 {
 		fmt.Println("Challenge needs to be 256 bits")
